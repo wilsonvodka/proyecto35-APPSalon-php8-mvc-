4b9f8c6fe2d060278ca5b68c2e71fcb45c8e679e
wilson alexander <wilgm0kdev@gmail.com>
2026-06-26 13:22:18 -0600

    cambio menor

--- src/js/app.js
+++ src/js/app.js
@@ -118,7 +118,7 @@ function paginaSiguiente() {
 
 async function consultarAPI() {
     try {
-        const url = `/api/servicios`;
+        const url = '/api/servicios';
         const resultado = await fetch(url);
         const servicios = await resultado.json();
         mostrarServicios(servicios);
